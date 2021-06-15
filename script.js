994de9811874d47fefa75c1a84ff4183aee0c655
Joseph Vega <17323639+JoeyVega@users.noreply.github.com>
2021-06-15 11:08:08 -0400

    Project Submitted

---
+++ script.js
@@ -0,0 +1,52 @@
+const URL = "https://gist.githubusercontent.com/JoeyVega/53a4c51d711298baef50da99ab222164/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json";
+
+let quotes = [];
+
+function status(response) {
+  if (response.status >= 200 && response.status < 300) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
+
+function json(response) {
+  return response.json();
+}
+
+function setQuotes(data) {
+  quotes = [...data.quotes];
+}
+
+function displayQuote(quote) {
+  $("#text").text('"' + quote.quote + '"');
+  $("#author").text("- " + quote.author);
+}
+
+function getRandomQuote() {
+  let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+  displayQuote(randomQuote);
+  tweetQuote(randomQuote);
+}
+
+function tweetQuote(quote) {
+  var baseTweetURL = "https://twitter.com/intent/tweet?text=";
+  $("#tweet-quote").attr("href", baseTweetURL + quote.quote + quote.author);
+}
+
+function enableButtons() {
+  $("#new-quote").prop("disabled", false);
+  $("#new-quote").removeClass("disabled");
+  $("#tweet-button").prop("disabled", false);
+  $("#tweet-button").removeClass("disabled");
+}
+
+$(function () {
+  fetch(URL).
+  then(status).
+  then(json).
+  then(setQuotes).
+  then(getRandomQuote).
+  then(enableButtons);
+  $('#new-quote').on('click', getRandomQuote);
+});
